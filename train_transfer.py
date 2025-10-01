d26fb22ff3560d758f7db2598e5bf812861c639b
MariaPapado <maria.papadomanolaki@orbitaleye.nl>
2025-10-01 17:06:57 +0200

    Update train_transfer.py

--- train_transfer.py
+++ train_transfer.py
@@ -7,4 +7,9 @@ model.train(
     imgsz=640,
     epochs=50,
     batch=8,
+#    flipud=0.50,
+#    fliplr=0.50,
+#    augment=True,
+#    freeze=10,                               # optional: freeze backbone
+#    lr0=0.001, weight_decay=5e-4,
 )
