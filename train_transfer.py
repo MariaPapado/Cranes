96748084e068f19d448dc5b9c6c6b124a57c0193
MariaPapado <maria.papadomanolaki@orbitaleye.nl>
2025-10-01 16:44:05 +0200

    Update train_transfer.py

--- train_transfer.py
+++ train_transfer.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("./runs/obb/train_dota/weights/best.pt")              # pretrained OBB
+model = YOLO("./runs/obb/train_dota/weights/best.pt")              # pretrained model i trained on DOTA with train_on_dota.py
 #print(model)
 model.train(
     data="CRANES.yaml",
